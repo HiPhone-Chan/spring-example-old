b60bc6f7f066a9b73b574b3e7cf1bcc009bdc31b
c00251918 <c00251918@SZXY1C002519181.china.huawei.com>
2015-08-11 10:14:15 +0800

    add config for encodingfilter

--- src/main/java/com/chf/sample/spring/config/SpringConfig.java
+++ src/main/java/com/chf/sample/spring/config/SpringConfig.java
@@ -1,5 +1,8 @@
 package com.chf.sample.spring.config;
 
+import javax.servlet.Filter;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -7,15 +10,23 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 @EnableAspectJAutoProxy
 @EnableTransactionManagement
 @ComponentScan(basePackages = { "com.chf.sample.domain",
-		"com.chf.sample.spring" })
+        "com.chf.sample.spring" })
 @EnableScheduling
 @ImportResource("classpath:spring-application.xml")
 @PropertySource("classpath:myApplication.properties")
 public class SpringConfig {
 
+    @Bean
+    public Filter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
 }
